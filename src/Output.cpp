27a61cb75afc488df50f5ea0fad20af078532a54
Michael Jones <m.pricejones@gmail.com>
2009-12-21 11:47:37 +0000

    Added basic EXR output

--- src/Output.cpp
+++ src/Output.cpp
@@ -2,27 +2,30 @@
 
 #include <fstream>
 
+#include <ImfRgbaFile.h>
+
 
 void Output::write()
 {
-	std::ofstream out;
-	
-	out.open( "test.txt" );
-
 	Imath::V2i resolution = m_imagePlane.resolution();
+	Imf::Rgba* pixels = new Imf::Rgba[ resolution.x * resolution.y ];
 
 	for ( int i=0; i<resolution.x; ++i )
 	{
 		for ( int j=0; j<resolution.y; ++j )
 		{
-			if ( m_imagePlane.getColor( Coordinate( i, j ) ) == Imath::Color3f( 1.0, 0.0, 0.0 ) )
-				out << "# ";
-			else if ( m_imagePlane.getColor( Coordinate( i, j ) ) == Imath::Color3f( 0.0, 0.0, 1.0 ) )
-				out << ". ";
+			Imath::Color3f color = m_imagePlane.getColor( Coordinate( i, j ) );
+			Imf::Rgba& pixel = pixels[ j * resolution.x + i ];
+
+			pixel.r = color.x;
+			pixel.g = color.y;
+			pixel.b = color.z;
+			pixel.a = 1.0;
 		}
-		out << std::endl;
 	}
 
-	out.close();
+	Imf::RgbaOutputFile file( "test.exr", resolution.x, resolution.y, Imf::WRITE_RGBA );
+	file.setFrameBuffer( pixels, 1, resolution.x );
+	file.writePixels( resolution.y );
 }
 
